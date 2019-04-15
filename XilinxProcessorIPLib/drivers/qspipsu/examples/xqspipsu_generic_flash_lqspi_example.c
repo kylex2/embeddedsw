kylex2/embeddedsw

commit a1b09d5d02b94a7b05deeb1b70cd837d296ede00

qspipsu: Fixed compilation error on IAR compiler
This patch fixes compilation error in lqspi example on IAR compiler.

Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>

Acked-for-series: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

XilinxProcessorIPLib/drivers/qspipsu/examples/xqspipsu_generic_flash_lqspi_example.c
@@ -82,6 +82,8 @@
 * 1.9   akm 02/27/19 Added support for IS25LP128, IS25WP128, IS25LP256,
 *                     IS25WP256, IS25LP512, IS25WP512 Flash Devices
 * 1.9   akm 03/26/19 Fixed data alignment warnings on IAR compiler.
+* 1.9   akm 03/26/19 Fixed compilation error in XQspiPsu_LqspiRead()
+*                     function on IAR compiler.
 *
 *</pre>
 *
@@ -673,7 +675,6 @@ int main(void)
 int XQspiPsu_LqspiRead(XQspiPsu *InstancePtr, u8 *RecvBufPtr, u32 Address,
 			unsigned ByteCount)
 {
-	void *ReadAddress = NULL;
 
 	Xil_AssertNonvoid(InstancePtr != NULL);
 	Xil_AssertNonvoid(RecvBufPtr != NULL);
@@ -686,9 +687,7 @@ int XQspiPsu_LqspiRead(XQspiPsu *InstancePtr, u8 *RecvBufPtr, u32 Address,
 
 	if (XQspiPsu_GetLqspiConfigReg(InstancePtr) &
 			XQSPIPSU_LQSPI_CR_LINEAR_MASK) {
-		ReadAddress +=
-			(XPAR_PSU_QSPI_LINEAR_0_S_AXI_BASEADDR + Address);
-		memcpy(ReadBuffer, ReadAddress, ByteCount);
+		memcpy((void *)ReadBuffer, (const void *)(XPAR_PSU_QSPI_LINEAR_0_S_AXI_BASEADDR + Address), (size_t)BiyteCount);
 		return XST_SUCCESS;
 	} else {
 		return XST_FAILURE;

XilinxProcessorIPLib/drivers/qspipsu/src/xqspipsu.h
@@ -160,6 +160,8 @@
  * 1.9 akm 03/08/19 Set recommended clock and data tap delay values for 40MHZ,
  *                  100MHZ and 150MHZ frequencies(CR#1023187)
  * 1.9   akm 03/26/19 Fixed data alignment warnings on IAR compiler.
+ * 1.9   akm 03/26/19 Fixed compilation error in XQspiPsu_LqspiRead()
+ *                     function on IAR compiler.
  *
  * </pre>
  *

commit 1962ead40d242df9544ed617f43917de5a5136f2

qspipsu: Fixed compilation error
Fixed compilation error in lqspi example.
This error got introduced with a1b09d5d02b94a7b05deeb1b70cd837d296ede00

Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>

Acked-for-series: Naga Sureshkumar Relli <nagasure@xilinx.com>

XilinxProcessorIPLib/drivers/qspipsu/examples/xqspipsu_generic_flash_lqspi_example.c
@@ -84,6 +84,7 @@
 * 1.9   akm 03/26/19 Fixed data alignment warnings on IAR compiler.
 * 1.9   akm 03/26/19 Fixed compilation error in XQspiPsu_LqspiRead()
 *                     function on IAR compiler.
+* 1.9   akm 04/12/19 Fixed compilation error in XQspiPsu_LqspiRead() function.
 *
 *</pre>
 *
@@ -687,7 +688,7 @@ int XQspiPsu_LqspiRead(XQspiPsu *InstancePtr, u8 *RecvBufPtr, u32 Address,
 
 	if (XQspiPsu_GetLqspiConfigReg(InstancePtr) &
 			XQSPIPSU_LQSPI_CR_LINEAR_MASK) {
-		memcpy((void *)ReadBuffer, (const void *)(XPAR_PSU_QSPI_LINEAR_0_S_AXI_BASEADDR + Address), (size_t)BiyteCount);
+		memcpy((void *)ReadBuffer, (const void *)(XPAR_PSU_QSPI_LINEAR_0_S_AXI_BASEADDR + Address), (size_t)ByteCount);
 		return XST_SUCCESS;
 	} else {
 		return XST_FAILURE;

XilinxProcessorIPLib/drivers/qspipsu/src/xqspipsu.h
@@ -162,6 +162,7 @@
  * 1.9   akm 03/26/19 Fixed data alignment warnings on IAR compiler.
  * 1.9   akm 03/26/19 Fixed compilation error in XQspiPsu_LqspiRead()
  *                     function on IAR compiler.
+ * 1.9   akm 04/12/19 Fixed compilation error in XQspiPsu_LqspiRead() function.
  *
  * </pre>
  *